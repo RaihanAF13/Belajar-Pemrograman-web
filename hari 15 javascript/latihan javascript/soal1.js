ad1a11fc92e7d26c5851283a1a9ca1ed275e4260
Raihan Anugrah Fakhri <163812179+RaihanAF13@users.noreply.github.com>
2026-03-20 17:14:45 +0700

    Add files via upload

---
+++ hari 15 javascript/latihan javascript/soal1.js
@@ -0,0 +1,6 @@
+let nama = "Raihan";
+let umur = 21;
+let hobi = "Workout";
+
+console.log("Nama saya " + nama + ", umur saya " + umur + " tahun, dan hobi saya adalah " + hobi + ".");
+
---
+++ hari 15 javascript/latihan javascript/soal2.js
@@ -0,0 +1,4 @@
+let panjang = prompt("Isikan panjang persegi panjang:");
+let lebar = prompt("Isikan lebar persegi panjang:");
+ let luas = panjang * lebar;
+ console.log("Luas persegi panjang adalah " + luas + ".");
